c326e33729deba2405d911a0aec905c93dfb2126
Tom3 <Tom3@Tom3-PC.lan>
2014-02-01 22:20:13 +0000

    Continuação do processo de internacionalização nas páginas:

--- GP_CRM/lang/pt_pt.php
+++ GP_CRM/lang/pt_pt.php
@@ -50,6 +50,112 @@ $lang_main_footer_text_3 = 'Código licenciado pela licença';
 $lang_main_footer_title_4 = 'Ver termos e condições para o uso, reprodução e distribuição da licensa Apache 2.0';
 $lang_main_footer_text_5 = 'Versão';
 
+//application/index.html
+$lang_application_index_text_1 = 'Painel de Administração';
+$lang_application_index_text_2 = 'Aplicação';
+$lang_application_index_text_3 = 'Gestão de Aplicação';
+$lang_application_index_text_4 = 'O módulo de gestão da Aplicação tem como principal funcionalidade gerir a informação referente à empresa, ou seja, alteração da mesma.';
+$lang_application_index_text_5 = 'Dados da empresa';
+$lang_application_index_text_6 = 'Nome';
+$lang_application_index_placeholder_7 = 'Nome da empresa';
+$lang_application_index_title_8 = 'Introduza o nome da empresa';
+$lang_application_index_text_9 = 'Introduza o nome da empresa';
+$lang_application_index_text_10 = 'Guardar';
+
 //worker/index.html
-$lang_worker_index_text_1 = '';
+$lang_worker_index_text_1 = 'Painel de Administração';
+$lang_worker_index_text_2 = 'Funcionários';
+$lang_worker_index_text_3 = 'Gestão de Funcionários';
+$lang_worker_index_text_4 = 'O módulo de gestão de funcionários tem como principal funcionalidade gerir a informação relacionada com os mesmos, ou seja, criar de novos funcionários, ver e alterar toda a informação associada às permissões dos funcionário e desativar e reativar os funcionários.';
+$lang_worker_index_placeholder_5 = 'Pesquisar';
+$lang_worker_index_text_6 = 'Número';
+$lang_worker_index_text_7 = 'Nome';
+$lang_worker_index_text_8 = 'Permissões';
+$lang_worker_index_text_9 = 'Todos os funcionários';
+$lang_worker_index_text_10 = 'Só funcionários ativos';
+$lang_worker_index_text_11 = 'Só funcionários desativos';
+$lang_worker_index_title_12 = 'Pesquisar';
+$lang_worker_index_text_13 = 'Pesquisar';
+$lang_worker_index_title_14 = 'Inserir novo funcionário';
+$lang_worker_index_text_15 = 'Novo funcionário';
+$lang_worker_index_text_16 = 'Número';
+$lang_worker_index_text_17 = 'Nome';
+$lang_worker_index_text_18 = 'Correio-eletrónico';
+$lang_worker_index_text_19 = 'Permissões';
+$lang_worker_index_text_20 = 'Opções';
+$lang_worker_index_text_21 = 'Ver';
+$lang_worker_index_text_22 = 'Alterar';
+$lang_worker_index_text_23 = 'Desativar';
+$lang_worker_index_text_24 = 'Reativar';
+$lang_worker_index_title_25 = 'Importar dados dos funcionários';
+$lang_worker_index_text_26 = 'Importar dados dos funcionários';
+$lang_worker_index_title_27 = 'Exportar dados dos funcionários';
+$lang_worker_index_text_28 = 'Exportar dados dos funcionários';
+
+
+//worker/insert.html
+$lang_worker_insert_text_1 = 'Painel de Administração';
+$lang_worker_insert_text_2 = 'Funcionários';
+$lang_worker_insert_text_3 = 'Inserir';
+$lang_worker_insert_text_4 = 'Inserir Funcionário';
+$lang_worker_insert_text_5 = 'Introduza os dados pessoais do funcionário, bem como as suas permissões.';
+$lang_worker_insert_text_6 = 'Dados Pessoais';
+$lang_worker_insert_text_7 = 'Número do funcionário';
+$lang_worker_insert_text_8 = 'Nome';
+$lang_worker_insert_placeholder_9 = 'Nome do funcionário';
+$lang_worker_insert_title_10 = 'Introduza o nome do funcionário';
+$lang_worker_insert_text_11 = 'Introduza o nome do funcionário';
+$lang_worker_insert_text_12 = 'Correio eletrónico';
+$lang_worker_insert_placeholder_13 = 'Correio eletrónico do funcionário';
+$lang_worker_insert_title_14 = 'Introduza o correio eletrónico do funcionário';
+$lang_worker_insert_text_15 = 'Introduza o correio eletrónico do funcionário';
+$lang_worker_insert_text_16 = 'Fotografia';
+$lang_worker_insert_title_17 = 'Introduza uma fotografia do funcionário';
+$lang_worker_insert_text_18 = 'Introduza uma fotografia do funcionário';
+$lang_worker_insert_text_19 = 'Permissões';
+$lang_worker_insert_text_20 = 'Escolha as permissões de acesso do funcionário';
+$lang_worker_insert_text_21 = 'Inserir';
+$lang_worker_insert_text_22 = 'Editar';
+$lang_worker_insert_text_23 = 'Desativar';
+$lang_worker_insert_text_24 = 'Gestão de';
+$lang_worker_insert_text_25 = ' Leads';
+$lang_worker_insert_text_26 = 'Gestão de Clientes';
+$lang_worker_insert_text_27 = 'Gestão de Campanhas';
+$lang_worker_insert_text_28 = 'Gestão de Encomendas';
+$lang_worker_insert_text_29 = 'Gestão de Funcionário';
+$lang_worker_insert_text_30 = 'Criar';
+
+//personal/index.html
+$lang_personal_index_text_1 = 'Painel de Administração';
+$lang_personal_index_text_2 = 'Área Pessoal';
+$lang_personal_index_text_3 = 'Gestão Área Pessoal';
+$lang_personal_index_text_4 = 'O módulo de gestão da Área Pessoal serve para alterar os dados de perfil.';
+$lang_personal_index_text_5 = 'Dados Pessoais';
+$lang_personal_index_text_6 = 'Nome';
+$lang_personal_index_placeholder_7 = 'Nome do Administrador';
+$lang_personal_index_title_8 = 'Altere o nome do Administrador';
+$lang_personal_index_text_9 = 'Altere o nome do Administrador';
+$lang_personal_index_text_10 = 'Correio eletrónico';
+$lang_personal_index_placeholder_11 = 'Correio eletrónico do Administrador';
+$lang_personal_index_title_12 = 'Altere o correio eletrónico do Administrador';
+$lang_personal_index_text_13 = 'Altere o correio eletrónico do Administrador';
+$lang_personal_index_text_14 = 'Fotografia';
+$lang_personal_index_title_15 = 'Altere a fotografia do Administrador';
+$lang_personal_index_text_16 = 'Altere a fotografia do Administrador';
+$lang_personal_index_text_17 = 'Palavra-Passe';
+$lang_personal_index_text_18 = 'Nova Palavra-Passe';
+$lang_personal_index_placeholder_19 = 'Nova Palavra-Passe';
+$lang_personal_index_title_20 = 'Nova Palavra-Passe';
+$lang_personal_index_text_21 = 'Introduza Nova Palavra-Passe';
+$lang_personal_index_text_22 = 'Confirme Palavra-Passe';
+$lang_personal_index_placeholder_23 = 'Confirme Nova Palavra-Passe';
+$lang_personal_index_title_24 = 'Confirme Nova Palavra-Passe';
+$lang_personal_index_text_25 = 'Confirme a sua Nova Palavra-Passe';
+$lang_personal_index_text_26 = 'Palavra-Passe Atual';
+$lang_personal_index_placeholder_27 = 'Palavra-Passe Atual';
+$lang_personal_index_title_28 = 'Palavra-Passe Atual';
+$lang_personal_index_text_29 = 'Introduza a Palavra-Passe Atual';
+$lang_personal_index_text_30 = 'Guardar';
+
+
 ?>
