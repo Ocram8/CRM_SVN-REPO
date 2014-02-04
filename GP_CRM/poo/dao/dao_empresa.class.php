14a0b7abb642e030d6e8add7facebc647fd4b2d1
Zé António <Z Antnio@192.168.2.111>
2014-02-04 11:37:10 +0000

    Criação do metódo visualizarEmpresa

--- GP_CRM/poo/dao/dao_empresa.class.php
+++ GP_CRM/poo/dao/dao_empresa.class.php
@@ -14,5 +14,26 @@
 			
 			$result = $this->bd->query($sql);
 		}
+		
+		public function visualizarEmpresas() {
+			$sql = "SELECT * FROM EMPRESA";
+			
+			$result = $this->bd->query($sql);
+			
+			if($result->getNumRows() == 0) return 0;
+			
+			while($row = $result->getNext()){
+				$sql1 = "SELECT * FROM MORADA WHEN MOR_ID = ".$row[2];
+				
+				$result1 = $this->bd->query($sql1);
+				if($result1->getNumRows() == 0) return 0;
+				
+				$row1 = $result->getNext();
+				
+				$morada = new Morada($row1[0],$row);
+				
+				$this->empresas[] = new Empressa($row[0], $row[1], $row[3], $row[4], $row[5], $row[6], $row[2]);
+			}
+		}
 	}
 ?>
