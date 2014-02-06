682eeddb7fb5a6f1c6f395c52c989239fd844ae4
Soldier984 <godlike.mailbox@gmail.com>
2014-02-06 00:08:42 +0000

    actualização do login.php

--- GP_CRM/actions/login.php
+++ GP_CRM/actions/login.php
@@ -1,14 +1,14 @@
-<?php 
+<?php
+session_start();
 
+require '../lang/pt_pt.php';
 ?>
 <!DOCTYPE html>
-<html lang="pt-PT">
+<html lang="pt">
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="description" content="Página de autenticação">
-<meta name="author" content="José Monteiro">
-<link rel="shortcut icon" href="img/favicon.png">
 
 <title>CRM | Autenticação</title>
 
@@ -26,22 +26,42 @@
 </head>
 
 <body>
-
 	<div class="container">
-
-		<form class="form-signin">
+		<form class="form-signin" action="login.php" method="post">
 			<h2 class="form-signin-heading">Autenticação</h2>
-			<input type="text" class="form-control" placeholder="Correio eletrónico" title="Introduza o seu endereço de correio eletrónico" autofocus> <input type="password" class="form-control" placeholder="Palavra-passe" title="Introduza a sua palavra-passe de autenticação"> <label class="checkbox"> <input
-				type="checkbox" value="remember-me"> Lembra-te de mim
-			</label> <label class=""> <a>Esqueceu-se da palavra-passe?</a>
-			</label>
+			
+			<input class="form-control" type="text" name="email" placeholder="Correio eletrónico" title="Introduza o seu endereço de correio eletrónico" autofocus>
+			<input class="form-control" type="password" name="password" placeholder="Palavra-passe" title="Introduza a sua palavra-passe de autenticação">
+			
+			<!--  
+			<label class="checkbox"><input type="checkbox" value="remember-me">Lembra-te de mim</label> 
+			<label><a>Esqueceu-se da palavra-passe?</a></label>
+			-->
+			
 			<button class="btn btn-lg btn-primary btn-block" type="submit" title="Pressione para concluir a autenticação">Entrar</button>
 		</form>
-
+		
+		<div class=form-signin>
+			<h4>contas a testar:</h4>
+			<p>admin admin</p>
+			<p>worker worker</p>
+		</div>
+		
 	</div>
 	<!-- /container -->
 </body>
 </html>
 <?php 
-	
+	if(isset($_POST['email']) && isset($_POST['password'])){
+		if($_POST['email'] == 'admin' && $_POST['password'] == 'admin'){
+			$_SESSION['login'] = true;
+			$_SESSION['user'] = 'admin';
+		}
+		
+		if($_POST['email'] == 'worker' && $_POST['password'] == 'worker'){
+			$_SESSION['login'] = true;
+			$_SESSION['user'] = 'worker';
+		}
+		header ( "Location: ../index.php" );
+	}
 ?>
