067f8c5bd162b2be290fc55b0472ae345d364e0f
monteiroxxi <lei123002@alunos.estgoh.ipc.pt>
2014-02-18 15:31:44 +0000

    Atualização da action desactive_worker

---
+++ GP_CRM/poo/actions/desactive_worker.php
@@ -0,0 +1,22 @@
+<?php
+
+
+//invocar ficheiros .php pretendidos
+include("../../bd/simple_db.php");
+include("../dao/dao_funcionario.class.php");
+include("../objects/funcionario.class.php");
+
+$db=new SimpleDBIConnection();
+$estado=false;
+
+$id = $_POST["id"];
+
+$dao = new FuncionarioDAO($db);
+
+$func = $dao->get_funcionario($id);
+
+$func->setfunAtivo($estado);
+
+$dao->update_funcionario($func);
+
+?>
